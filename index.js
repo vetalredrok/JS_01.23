4c2cf173679f5ee43f149390d49f21ca60478b66
vetalredrok <vetalredrok@gmail.com>
2023-02-09 12:47:30 +0200

    [3.1,2:09.02]: finished 1 & 2 tasks

--- index.js
+++ index.js
@@ -1,76 +1,27 @@
-function makeDeepCopy(obj){
-    if (typeof obj !== "object" || obj === null) {
+Array.prototype.customFilter = function (callback, obj) {
+    if (typeof callback !== "function") {
         throw new Error();
     }
 
-    function deepCopy(obj) {
-        if (typeof obj !== "object" || obj === null) {
-            return obj;
-        }
-        if (Array.isArray(obj)) {
-            let result = [];
-            for (let i = 0; i < obj.length; i++) {
-                result[i] = deepCopy(obj[i]);
-            }
-            return result;
-        } else if (obj instanceof Set) {
-            let result = new Set();
-            for (let value of obj) {
-                result.add(deepCopy(value));
-            }
-            return result;
-        } else if (obj instanceof Map) {
-            let result = new Map();
-            for (let [key, value] of obj) {
-                result.set(deepCopy(key), deepCopy(value));
-            }
-            return result;
-        } else {
-            let result = {};
-            for (let key in obj) {
-                result[key] = deepCopy(obj[key]);
-            }
-            return result;
+    let linkToArray = this;
+    const result = [];
+
+    for (let i = 0; i < linkToArray.length; i++) {
+        if (callback.call(obj, linkToArray[i], i, linkToArray)) {
+            result.push(linkToArray[i]);
         }
     }
 
-    return deepCopy(obj);
-}
+    return result;
+};
 
-function selectFromInterval(arr, start, end) {
-    if (!Array.isArray(arr) || arr.some(el => typeof el !== 'number') || arr.length === 0 || arr.some(el => !isFinite(el))) {
-        throw new Error();
-    }
-    if (isNaN(start) || isNaN(end) || [start,end].some(el => !isFinite(el))) {
-        throw new Error();
-    }
-    let min = Math.min(start, end);
-    let max = Math.max(start, end);
-    return arr.filter(el => el >= min && el <= max);
-}
-
-
-function createIterable(from, to) {
-    if (typeof from !== "number" || typeof to !== "number" || from >= to || [from, to].some(el => !isFinite(el))) {
-        throw new Error();
-    }
-
-    return {
-        [Symbol.iterator]: function() {
-            let current = from;
-            return {
-                next: function() {
-                    if (current <= to) {
-                        return { value: current++, done: false };
-                    } else {
-                        return { done: true };
-                    }
-                }
-            };
-        }
+function createDebounceFunction(callback, pause) {
+    let myTime;
+    return function debounced() {
+        clearTimeout(myTime);
+        myTime = setTimeout(() => {
+            callback();
+        }, pause);
     };
 }
-
-
-
 
