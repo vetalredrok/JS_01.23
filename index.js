0196d7ba8e4a076cc5684334401b10861958d215
vetalredrok <vetalredrok@gmail.com>
2023-02-13 02:32:19 +0200

    [4.1,2,3:13.02]: finished 1 & 2 & 3 tasks

--- index.js
+++ index.js
@@ -1,27 +1,99 @@
-Array.prototype.customFilter = function (callback, obj) {
-    if (typeof callback !== "function") {
-        throw new Error();
-    }
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
-    let linkToArray = this;
-    const result = [];
-
-    for (let i = 0; i < linkToArray.length; i++) {
-        if (callback.call(obj, linkToArray[i], i, linkToArray)) {
-            result.push(linkToArray[i]);
+function curry(f){
+    return function curried(...args){
+        if (args.length >= f.length){
+            return f.apply(this, args);
+        } else {
+            return function iter(...argsNew){
+                return curried.apply(this, args.concat(argsNew));
+            }
         }
-    }
-
-    return result;
-};
-
-function createDebounceFunction(callback, pause) {
-    let myTime;
-    return function debounced() {
-        clearTimeout(myTime);
-        myTime = setTimeout(() => {
-            callback();
-        }, pause);
     };
 }
 
+class Calculator {
+    constructor(x, y) {
+        if (!this.isValidNumber(x) || !this.isValidNumber(y)) {
+            throw new Error('');
+        }
+        this.x = x;
+        this.y = y;
+    }
+
+    isValidNumber(num) {
+        return typeof num === 'number' && !isNaN(num) && isFinite(num);
+    }
+
+    setX = (num) => {
+        if (!this.isValidNumber(num)) {
+            throw new Error('');
+        }
+        this.x = num;
+    }
+
+    setY = (num) => {
+        if (!this.isValidNumber(num)) {
+            throw new Error('');
+        }
+        this.y = num;
+    }
+
+    getSum = () => {
+        return this.x + this.y;
+    }
+
+    getMul = () => {
+        return this.x * this.y;
+    }
+
+    getSub = () => {
+        return this.x - this.y;
+    }
+
+    getDiv = () => {
+        if (this.y === 0) {
+            throw new Error('');
+        }
+        return this.x / this.y;
+    }
+}
+
+
+class RickAndMorty {
+    constructor() {
+    }
+
+    getCharacter(id) {
+        if (!Number.isInteger(id) || id <= 0) {
+            throw new Error('');
+        }
+
+        return fetch(`https://rickandmortyapi.com/api/character/${id}`)
+            .then(res => {
+                if (res.status === 200) {
+                    return res.json();
+                }
+                return null;
+            })
+            .catch(e => {
+                return null;
+            });
+    }
+
+    async getEpisode(id) {
+        if (!Number.isInteger(id) || id <= 0) {
+            throw new Error('');
+        }
+
+        try {
+            const res = await fetch(`https://rickandmortyapi.com/api/episode/${id}`);
+            if (res.status === 200) {
+                return await res.json();
+            }
+            return null;
+        } catch (e) {
+            return null;
+        }
+    }
+}
